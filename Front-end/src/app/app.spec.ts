aea84230dc7e7bdd22b15552c27685000e6c1f22
Vander Costa <vander.costa@meta.com.br>
2025-09-08 19:20:27 -0300

    test(frontend): adjust unit test

--- Front-end/src/app/app.spec.ts
+++ Front-end/src/app/app.spec.ts
@@ -1,13 +1,22 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { AuthService } from './core/services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
 
 describe('App', () => {
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
+
   beforeEach(async () => {
+    const spy = jasmine.createSpyObj('AuthService', ['getAdmin']);
+
     await TestBed.configureTestingModule({
-      imports: [App],
-      providers: [provideZonelessChangeDetection()],
+      imports: [App, MatButtonModule],
+      providers: [{ provide: AuthService, useValue: spy }, provideZonelessChangeDetection()],
     }).compileComponents();
+
+    authServiceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    authServiceSpy.getAdmin.and.returnValue('false');
   });
 
   it('should create the app', () => {
